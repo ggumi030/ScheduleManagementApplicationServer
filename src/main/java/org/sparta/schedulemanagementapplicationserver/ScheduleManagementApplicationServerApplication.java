016adf50b489b1338abd944fd3592f6832fa96ea
ggumi030 <tndus50843@gmail.com>
2024-05-16 14:56:33 +0900

    init commit

---
+++ src/main/java/org/sparta/schedulemanagementapplicationserver/ScheduleManagementApplicationServerApplication.java
@@ -0,0 +1,13 @@
+package org.sparta.schedulemanagementapplicationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagementApplicationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleManagementApplicationServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sparta/schedulemanagementapplicationserver/ScheduleManagementApplicationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.schedulemanagementapplicationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManagementApplicationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
